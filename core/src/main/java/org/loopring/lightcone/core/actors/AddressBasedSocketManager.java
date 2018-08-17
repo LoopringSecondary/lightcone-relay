a0012f195f6dfa0cd24fa599e156cccaac337a90
mmmmmagina <jaice_229@163.com>
2018-08-17 08:51:55 +0800

    add proto design

---
+++ core/src/main/java/org/loopring/lightcone/core/actors/AddressBasedSocketManager.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.loopring.lightcone.core.actors;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import org.loopring.lightcone.proto.Cache;
+import org.loopring.lightcone.proto.Cache.CacheAddressBalanceInfo;
+import org.loopring.lightcone.proto.Cache.CachedAddressBalanceInfo;
+
+import java.util.Optional;
+
+public class AddressBasedSocketManager extends AbstractActor {
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    public static Props props(Optional<String> settingsId) {
+        return Props.create(AddressBasedSocketManager.class);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(CacheAddressBalanceInfo.class, r -> getSender().tell(CachedAddressBalanceInfo.getDefaultInstance(), getSender()))
+                .match(Cache.GetAddressBalanceFromCache.class, r -> getSender().tell(Cache.AddressBalanceFromCache.getDefaultInstance(), getSender()))
+                .match(Cache.PurgeTokenBalancesForAddresses.class, r -> {})
+                .match(Cache.PurgeEverything.class, r -> {})
+                .build();
+    }
+}
