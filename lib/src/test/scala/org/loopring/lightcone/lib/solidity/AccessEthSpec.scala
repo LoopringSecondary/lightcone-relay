9694bbbb743b7d0b1d81472270cfc97e1943fa45
dylen <dylenfu@126.com>
2018-08-20 11:22:03 +0800

    git add geth client

--- lib/src/test/scala/org/loopring/lightcone/lib/solidity/AccessEthSpec.scala
+++ lib/src/test/scala/org/loopring/lightcone/lib/solidity/AccessEthSpec.scala
@@ -1,22 +1,49 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.loopring.lightcone.lib.solidity
 
-/*
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import org.scalatest.FlatSpec
+import scala.util.{ Failure, Success }
+import scala.concurrent.Future
 
-  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+class AccessEthSpec extends FlatSpec {
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+  info("execute cmd [sbt lib/'testOnly *AccessEthSpec'] to test single spec of getEthBalance")
 
-  http://www.apache.org/licenses/LICENSE-2.0
+  val account = "0x4bad3053d574cd54513babe21db3f09bea1d387d"
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+  "getEthBalance" should "convert to two orders" in {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
 
-*/
-class AccessEthSpec {
+    val url = ""
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url))
+
+    responseFuture
+      .onComplete {
+        case Success(res) => println(res)
+        case Failure(_) => sys.error("something wrong")
+      }
+  }
 
 }
