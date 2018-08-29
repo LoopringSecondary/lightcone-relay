1bce92986b190314ba83afbfdf3a0e0984e1ace5
dylen <dylenfu@126.com>
2018-08-29 15:39:50 +0800

    handle mined transaction

--- core/src/main/scala/org/loopring/lightcone/core/actors/BlockchainEventExtractor.scala
+++ core/src/main/scala/org/loopring/lightcone/core/actors/BlockchainEventExtractor.scala
@@ -20,9 +20,11 @@ import akka.actor._
 import org.loopring.lightcone.core.accessor.EthClient
 import org.loopring.lightcone.core.actors.base.RepeatedJobActor
 import org.loopring.lightcone.proto.token._
+import org.loopring.lightcone.proto.block_chain_event._
 import org.loopring.lightcone.proto.eth_jsonrpc._
 import org.loopring.lightcone.proto.deployment._
 import org.loopring.lightcone.lib.solidity.Abi
+import org.loopring.lightcone.proto.solidity._
 
 import scala.concurrent.Future
 
@@ -36,6 +38,8 @@ object BlockchainEventExtractor
   def getCommon(s: BlockchainEventExtractorSettings) =
     base.CommonSettings("", s.roles, 1)
 }
+
+case class MinedTransaction(receipt: TransactionReceipt, trace: TraceTransaction)
 
 class BlockchainEventExtractor(
   val tokenList: Seq[Token],
@@ -58,24 +62,53 @@ class BlockchainEventExtractor(
   // todo: get protocol address(delegate, impl, token register...) on chain
   val supportedContracts: Seq[String] = tokenList.map(x => safeAddress(x.protocol))
 
-  // todo: 首次从数据库获取blockNumber,后续启动从数据库获取blockNumber
-  val currentBlock: BigInt = ???
-
   override def receive: Receive = {
     case settings: BlockchainEventExtractorSettings =>
   }
 
-  // get block transactions receipts
-  // fork detecting
-  // decode methods and events
-  // send data
-  override def handleRepeatedJob(): Future[Unit] = ???
+  // 每次处理一个块,
+  // 从block里获取对应的transaction hash
+  // 获取transactionReceipt以及对应的trace数据
+  // 解析后将数据打包成多个完整的transaction发送出去
+  override def handleRepeatedJob() = for {
+    currentBlockNumber <- getCurrentBlock()
+    blockReq = GetBlockWithTxHashByNumberReq(currentBlockNumber)
+    block <- accessor.getBlockWithTxHashByNumber(blockReq)
+    minedTxs <- getMinedTransactions(block.getResult.transactions)
+    _ <- minedTxs.map(handleMinedTransaction(_))
+    // handlePendingTransaction()
+  } yield None
 
-  // todo
-  def handleSingleTransaction() = ???
+  def handleMinedTransaction(tx: MinedTransaction) = {
+    val list = unpackMinedTransaction(tx)
+    list.map(route(_))
+  }
 
-  // todo: decode input or event data and convert to onchain event
-  def unpack(): AnyRef = ???
+  def handlePendingTransaction() = ???
+
+  def getMinedTransactions(hashseq: Seq[String]): Future[Seq[MinedTransaction]] = Future.sequence(
+    hashseq.map(txhash =>
+    for {
+      receipt <- accessor.getTransactionReceipt(GetTransactionReceiptReq(txhash))
+      trace <- accessor.traceTransaction(TraceTransactionReq(txhash))
+    } yield MinedTransaction(receipt.getResult, trace.getResult)
+  ))
+
+  def getPendingTransactions(hashSeq: Seq[String]): Future[Seq[Transaction]] = ???
+  
+  // todo: 解析receipt, trace等并转换成blockChainEvent
+  def unpackMinedTransaction(tx: MinedTransaction): Seq[Any] = ???
+
+  def unpackPendingTransaction(tx: Transaction): Seq[Any] = ???
+
+  def route(onchainEvent: Any) = onchainEvent match {
+    case balance: SubmitRingFunction =>
+    case ring : RingDetectedInMemPoll =>
+    case ringMined: RingMined =>
+    case cancel: OrderCancelled =>
+    case cutoff: Cutoff =>
+    case cutoffPair: CutoffPair =>
+  }
 
   def isSupportedFunction(txTo: String, txInput: String): Boolean = {
     require(isProtocolSupported(txTo))
@@ -88,6 +121,11 @@ class BlockchainEventExtractor(
     val eventId = getEventId(firstTopic)
     abiEvents.contains(eventId)
   }
+
+// todo: 首次从数据库获取blockNumber,后续启动从数据库获取blockNumber
+  private def  getCurrentBlock(): Future[String] = for {
+    _ <- Future{}
+  } yield "0x32"
 
   private def isProtocolSupported(txTo: String): Boolean = supportedContracts.contains(safeAddress(txTo))
 
