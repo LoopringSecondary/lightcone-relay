53a10a8dcdebcd3af05020b76e5095f955647767
于红雨 <yuhongyu04@126.com>
2018-08-30 17:29:01 +0800

    implement submitter

--- core/src/main/scala/org/loopring/lightcone/core/actors/RingSubmitter.scala
+++ core/src/main/scala/org/loopring/lightcone/core/actors/RingSubmitter.scala
@@ -16,10 +16,20 @@
 
 package org.loopring.lightcone.core.actors
 
+import java.math.BigInteger
+
 import akka.actor.Actor
 import akka.util.Timeout
 import org.loopring.lightcone.proto.deployment.RingSubmitterSettings
-import org.web3j.crypto.{Credentials, WalletUtils}
+import org.loopring.lightcone.proto.ring.RingCandidates
+import org.web3j.crypto.{ Credentials, RawTransaction, TransactionEncoder, WalletUtils }
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.{ DefaultBlockParameter, Request }
+import org.web3j.protocol.core.methods.request.{ EthFilter, ShhFilter, ShhPost, Transaction }
+import org.web3j.protocol.core.methods.response
+import org.web3j.protocol.core.methods.response._
+import org.web3j.tx.ChainId
+import rx.Observable
 
 import scala.concurrent.ExecutionContext
 
@@ -38,7 +48,8 @@ class RingSubmitter()(implicit
   extends Actor {
 
   var submitterCredentials = Map[String, Credentials]()
-
+  var chainId = 7107171.toByte
+  var contract = ""
   override def receive: Receive = {
     case settings: RingSubmitterSettings =>
       submitterCredentials = settings.keystoreFiles.map { k =>
@@ -46,6 +57,22 @@ class RingSubmitter()(implicit
         (credential.getAddress, credential)
       }.toMap
 
-    case _ =>
+    case ringToSettle: RingCandidates =>
+      val inputData = ""
+      val rawTransaction = RawTransaction.createTransaction(
+        BigInt(0).bigInteger,
+        BigInt(0).bigInteger,
+        BigInt(0).bigInteger,
+        contract,
+        BigInt(0).bigInteger,
+        inputData)
+      val signedMessage = signTransaction(rawTransaction, submitterCredentials(""))
+  }
+
+  def signTransaction(rawTransaction: RawTransaction, credentials: Credentials) = {
+    if (chainId > ChainId.NONE)
+      TransactionEncoder.signMessage(rawTransaction, chainId, credentials)
+    else
+      TransactionEncoder.signMessage(rawTransaction, credentials)
   }
 }
