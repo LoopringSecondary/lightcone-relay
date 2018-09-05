7f8c1e394741c862a4b277662fdf22a8aeb011fb
hongyu <yuhongyu04@126.com>
2018-09-05 23:29:19 +0800

    implement evaluator (#40)

--- core/src/main/scala/org/loopring/lightcone/core/actors/RingMiner.scala
+++ core/src/main/scala/org/loopring/lightcone/core/actors/RingMiner.scala
@@ -19,13 +19,16 @@ package org.loopring.lightcone.core.actors
 import akka.actor._
 import akka.pattern._
 import akka.util.Timeout
-import scala.concurrent.ExecutionContext
+import org.loopring.lightcone.core.accessor.EthClient
 import org.loopring.lightcone.core.actors.base._
 import org.loopring.lightcone.core.routing._
+import org.loopring.lightcone.core.utils._
+import org.loopring.lightcone.lib.math.Rational
 import org.loopring.lightcone.proto.deployment._
-import org.loopring.lightcone.proto.common._
+import org.loopring.lightcone.proto.order.OrderSettleAmount
 import org.loopring.lightcone.proto.ring._
-import scala.concurrent.Future
+
+import scala.concurrent.{ ExecutionContext, Future }
 
 object RingMiner
   extends base.Deployable[RingMinerSettings] {
@@ -36,29 +39,82 @@ object RingMiner
     base.CommonSettings(Some(s.address), s.roles, 1)
 }
 
-class RingMiner()(implicit timout: Timeout)
+class RingMiner(ethClient: EthClient)(implicit
+  ec: ExecutionContext,
+  timeout: Timeout)
   extends RepeatedJobActor {
-
-  import context.dispatcher
-  var finders: Seq[ActorRef] = Seq.empty
-  lazy val balanceManager = Routers.balanceManager
-  lazy val ethereumAccessor = Routers.ethereumAccessor
+  val lrcAddress = "0xef68e7c694f40c8202821edf525de3782458639f"
+  var marketIds = Seq[String]()
+  var submitter: RingSubmitter = null
+  var evaluator: RingEvaluator = null
+  var finders = Map[String, ActorRef]()
 
   override def receive: Receive = super.receive orElse {
     case settings: RingMinerSettings =>
-      finders = settings.marketIds.map { id => Routers.ringFinder(id) }
+      marketIds = settings.marketIds
+      settings.submitterSettings match {
+        case Some(submitterSettings) =>
+          submitter = new RingSubmitterImpl(
+            ethClient = ethClient,
+            contract = submitterSettings.contract,
+            chainId = submitterSettings.chainId.toByte,
+            keystorePwd = submitterSettings.keystorePwd,
+            keystoreFile = submitterSettings.keystoreFile)
+        case None =>
+      }
+      settings.evaluatorSettings match {
+        case Some(evaluatorSettings) =>
+          evaluator = new RingEvaluatorImpl(
+            submitterAddress = submitter.getSubmitterAddress(),
+            lrcAddress = lrcAddress,
+            gasUsedOfOrders = evaluatorSettings.gasUsedOfOrders,
+            walletSplit = Rational(evaluatorSettings.walletSplit))
+        case None =>
+      }
+      finders = marketIds.map { id => (id, Routers.ringFinder(id)) }.toMap[String, ActorRef]
       initAndStartNextRound(settings.scheduleDelay)
   }
 
   def handleRepeatedJob() = for {
-    resps <- Future.sequence(finders.map { _ ? GetRingCandidates() })
-      .mapTo[Seq[RingCandidates]]
+    //finderid以及其返回的RingCandidates
+    resps <- Future.sequence(finders.map { finder =>
+      for {
+        resp <- finder._2 ? GetRingCandidates()
+      } yield (finder._1, resp)
+    }).mapTo[Seq[(String, RingCandidates)]]
+    //记录ring与finderid的对应关系
+    ringToFinderMap = resps.flatMap(resp => resp._2.rings.map(ring => (ring.hash, resp._1))).toMap
+    ringCandidates = RingCandidates(resps.flatMap(_._2.rings))
+    ringsToSettle <- evaluator.getRingCadidatesToSettle(ringCandidates)
   } yield {
-    resps.map(_.rings).flatten
+    decideRingCandidates(ringCandidates.rings, ringsToSettle)
+      .groupBy { decision =>
+        ringToFinderMap(decision.ringHash) //按照finderid分开，将对应的decision返回给各自的finder
+      } foreach { decision =>
+        val finder = Routers.ringFinder(decision._1)
+        finder ! NotifyRingSettlementDecisions(decision._2)
+      }
+    ringsToSettle.foreach(submitter.signAndSendTx)
   }
 
-  def decideRingCandidates(ring: Seq[Ring]): NotifyRingSettlementDecisions = {
-    NotifyRingSettlementDecisions()
+  def decideRingCandidates(ringCandidates: Seq[Ring], settledRings: Seq[RingCandidate]): Seq[RingSettlementDecision] = {
+    val settledRingHashSet = settledRings.map(_.rawRing.hash).toSet
+    ringCandidates.map { candidate =>
+      if (settledRingHashSet.contains(candidate.hash))
+        RingSettlementDecision(
+          ringHash = candidate.hash,
+          decision = SettlementDecision.Settled,
+          ordersSettleAmount = candidate.orders.map { o =>
+            OrderSettleAmount(orderHash = o.rawOrder.get.hash, amount = o.dealtAmountS)
+          })
+      else
+        RingSettlementDecision(
+          ringHash = candidate.hash,
+          decision = SettlementDecision.UnSettled,
+          ordersSettleAmount = candidate.orders.map { o =>
+            OrderSettleAmount(orderHash = o.rawOrder.get.hash, amount = o.dealtAmountS)
+          })
+    }
   }
-
 }
+
