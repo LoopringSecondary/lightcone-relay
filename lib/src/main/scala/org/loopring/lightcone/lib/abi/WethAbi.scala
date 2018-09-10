db8e42f44134a7a0435eac06207910f113c532b9
dylen <dylenfu@126.com>
2018-09-10 18:41:42 +0800

    add weth abi

--- lib/src/main/scala/org/loopring/lightcone/lib/abi/WethAbi.scala
+++ lib/src/main/scala/org/loopring/lightcone/lib/abi/WethAbi.scala
@@ -16,7 +16,102 @@
 
 package org.loopring.lightcone.lib.abi
 
-// todo
-class WethAbi {
+import com.google.inject.Inject
+import com.typesafe.config.Config
+import org.loopring.lightcone.lib.solidity.Abi
+import org.loopring.lightcone.proto.eth_jsonrpc.Log
+import org.loopring.lightcone.proto.block_chain_event._
+
+class WethAbi @Inject() (config: Config) extends ContractAbi {
+
+  val FN_DEPOSIT = "deposit"
+  val FN_WITHDRAW = "withdraw"
+
+  val EN_DEPOSIT = "Deposit"
+  val EN_WITHDRAWAL = "Withdrawal"
+
+  override def abi: Abi = Abi.fromJson(config.getString("abi.weth"))
+  override val supportedFunctions: Seq[String] = Seq(
+    FN_DEPOSIT, FN_WITHDRAW)
+  override val supportedEvents: Seq[String] = Seq(
+    EN_DEPOSIT, EN_WITHDRAWAL)
+
+  override val sigFuncMap: Map[String, Abi.Function] = super.sigFuncMap
+  override val sigEvtMap: Map[String, Abi.Event] = super.sigEvtMap
+  override val nameFuncMap: Map[String, Abi.Function] = super.nameFuncMap
+  override val nameEvtMap: Map[String, Abi.Event] = super.nameEvtMap
+
+  def decodeInputAndAssemble(input: String, header: TxHeader): Seq[Any] = {
+    val res = decodeInput(input)
+    res.name match {
+      case FN_DEPOSIT => Seq(assembleDepositFunction(res.list, header))
+      case FN_WITHDRAW => Seq(assembleWithdrawalFunction(res.list, header))
+      case _ => Seq()
+    }
+  }
+
+  def decodeLogAndAssemble(log: Log, header: TxHeader): Seq[Any] = {
+    val res = decodeLog(log)
+    res.name match {
+      case EN_DEPOSIT => Seq(assembleDepositEvent(res.list, header))
+      case EN_WITHDRAWAL => Seq(assembleWithdrawalEvent(res.list, header))
+      case _ => Seq()
+    }
+  }
+
+  // function deposit() public payable
+  def assembleDepositFunction(list: Seq[Any], header: TxHeader): WethDeposit = {
+    val ret = WethDeposit()
+      .withOwner(header.from)
+      .withValue(header.value)
+      .withTxHeader(header)
+
+    print(ret.toProtoString)
+
+    ret
+  }
+
+  // event  Deposit(address indexed dst, uint wad);
+  def assembleDepositEvent(list: Seq[Any], header: TxHeader): WethDeposit = {
+    val ret = WethDeposit()
+      .withOwner(scalaAny2Hex(list(0)))
+      .withValue(scalaAny2Bigint(list(1)).toString())
+      .withTxHeader(header)
+
+    print(ret.toProtoString)
+
+    ret
+  }
+
+  // function withdraw(uint wad) public
+  def assembleWithdrawalFunction(list: Seq[Any], header: TxHeader): WethWithdrawal = {
+    if (list.length != 1) {
+      throw new Exception("withdrawal function list length invalid")
+    }
+
+    val ret = WethWithdrawal()
+      .withOwner(header.from)
+      .withValue(scalaAny2Bigint(list(0)).toString())
+      .withTxHeader(header)
+
+    print(ret)
+
+    ret
+  }
+
+  // event  Withdrawal(address indexed src, uint wad);
+  def assembleWithdrawalEvent(list: Seq[Any], header: TxHeader): WethWithdrawal = {
+    if (list.length != 2) {
+      throw new Exception("withdrawal event list length invalid")
+    }
+
+    val ret = WethWithdrawal()
+      .withOwner(scalaAny2Hex(list(0)))
+      .withValue(scalaAny2Bigint(list(1)).toString())
+      .withTxHeader(header)
+
+    print(ret)
+    ret
+  }
 
 }
