e30b813842ddf7bbf0ce2801ff1f58acc7143eaf
Daniel Wang <dong77@gmail.com>
2018-09-11 00:45:46 -0400

    refactor (#61)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,6 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
     "com.typesafe.akka" %% "akka-contrib" % akkaVersion,
     "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
-    // "com.typesafe.akka" %% "akka-cluster-typed" % akkaVersion,
     "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
     "com.typesafe.akka" %% "akka-cluster-metrics" % akkaVersion,
     "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
