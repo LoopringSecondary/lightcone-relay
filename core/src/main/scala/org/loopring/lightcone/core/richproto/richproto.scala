f5e5181dcb7dbbb9daf57dfbe9cc49871d3dd1fa
于红雨 <yuhongyu04@126.com>
2018-09-18 15:04:07 +0800

    impl gethash and signerAddr of richorder

--- core/src/main/scala/org/loopring/lightcone/core/richproto/richproto.scala
+++ core/src/main/scala/org/loopring/lightcone/core/richproto/richproto.scala
@@ -19,9 +19,12 @@ package org.loopring.lightcone.core
 import org.loopring.lightcone.lib.math.Rational
 import org.loopring.lightcone.proto.order.Order
 import org.loopring.lightcone.lib.etypes._
+import org.web3j.crypto._
+import org.web3j.utils.Numeric
 
 package object richproto {
   implicit class RichOrder(order: Order) {
+
     def getRemained(): (Rational, Rational) = {
       if (order.rawOrder.get.buyNoMoreThanAmountB) {
         val sellPrice = order.sellPrice()
@@ -46,14 +49,43 @@ package object richproto {
       }
     }
 
-    def getSignerAddr(): String = ???
+    def getSignerAddr(): String = {
+      val rawOrder = order.rawOrder.get
+      val orderHash = Numeric.hexStringToByteArray(order.rawOrder.get.hash)
+      val hash = Hash.sha3(("\u0019Ethereum Signed Message:\n" + orderHash.length).getBytes() ++ orderHash)
+      val publicKey = Sign.recoverFromSignature(
+        (rawOrder.v - 27).toByte,
+        new ECDSASignature(rawOrder.r.asBigInteger, rawOrder.s.asBigInteger),
+        hash)
+      Keys.getAddress(publicKey)
+    }
 
     def sellPrice(): Rational = {
       Rational(order.rawOrder.get.amountS.asBigInt, order.rawOrder.get.amountB.asBigInt)
     }
+
     def buyPrice(): Rational = {
       Rational(order.rawOrder.get.amountB.asBigInt, order.rawOrder.get.amountS.asBigInt)
     }
-  }
 
+    def getHash():String = {
+      val rawOrder = order.rawOrder.get
+      val buyNoMoreThanB = if (rawOrder.buyNoMoreThanAmountB) 1 else 0
+
+      val data = Numeric.hexStringToByteArray(rawOrder.delegateAddress) ++
+        Numeric.hexStringToByteArray(rawOrder.owner) ++
+        Numeric.hexStringToByteArray(rawOrder.tokenS) ++
+        Numeric.hexStringToByteArray(rawOrder.tokenB) ++
+        Numeric.hexStringToByteArray(rawOrder.walletAddress) ++
+        Numeric.hexStringToByteArray(rawOrder.authAddr) ++
+        Numeric.toBytesPadded(rawOrder.amountS.asBigInteger, 32) ++
+        Numeric.toBytesPadded(rawOrder.amountB.asBigInteger, 32) ++
+        Numeric.toBytesPadded(BigInt(rawOrder.validSince).bigInteger, 32) ++
+        Numeric.toBytesPadded(BigInt(rawOrder.validUntil).bigInteger, 32) ++
+        Numeric.toBytesPadded(rawOrder.lrcFee.asBigInteger, 32) ++
+        Array[Byte](buyNoMoreThanB.toByte, rawOrder.marginSplitPercentage.toByte)
+
+      Numeric.toHexString(Hash.sha3(data))
+    }
+  }
 }
