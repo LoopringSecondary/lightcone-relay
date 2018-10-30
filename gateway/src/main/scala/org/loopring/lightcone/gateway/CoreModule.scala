6d780488b61a2ea59b0aa3d7aaa165d4d7f47682
Toan <lelence@163.com>
2018-10-30 22:30:18 +0800

    update gateway run and injector

--- gateway/src/main/scala/org/loopring/lightcone/gateway/CoreModule.scala
+++ gateway/src/main/scala/org/loopring/lightcone/gateway/CoreModule.scala
@@ -1,5 +1,49 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.loopring.lightcone.gateway
 
-class CoreModule {
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.google.inject.AbstractModule
+import com.typesafe.config.Config
+import net.codingwell.scalaguice.ScalaModule
+import org.loopring.lightcone.gateway.api.service.{ BalanceService, BalanceServiceImpl }
+
+object CoreModule {
+
+  def apply(implicit system: ActorSystem): CoreModule = new CoreModule
+
+}
+
+class CoreModule(implicit system: ActorSystem)
+  extends AbstractModule with ScalaModule {
+
+  override def configure(): Unit = {
+
+    bind[ActorSystem].toInstance(system)
+
+    bind[Config].toInstance(system.settings.config)
+
+    bind[ActorMaterializer].toInstance(ActorMaterializer())
+
+    // bind[ExecutionContext].toInstance(system.dispatcher)
+
+    bind[BalanceService].to[BalanceServiceImpl]
+
+  }
 
 }
--- gateway/src/main/scala/org/loopring/lightcone/gateway/Main.scala
+++ gateway/src/main/scala/org/loopring/lightcone/gateway/Main.scala
@@ -1,5 +1,37 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.loopring.lightcone.gateway
 
-class Main {
+import akka.actor.ActorSystem
+import com.google.inject.Guice
+import com.typesafe.config.ConfigFactory
+import org.loopring.lightcone.gateway.api.service.BalanceService
+import org.loopring.lightcone.gateway.socketio.{ SocketIOSettings, SocketIOSystemExtension }
+
+object Main extends App {
+
+  implicit val system = ActorSystem("Api", ConfigFactory.load())
+
+  val injector = Guice.createInjector(CoreModule(system))
+
+  // socketio server
+  // register [trait or interface]
+  val settings = SocketIOSettings().register[BalanceService]
+  val server = SocketIOSystemExtension(system).init(injector, settings)
+  server.start
 
 }
